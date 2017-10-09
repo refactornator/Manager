db7ebc4adee7c13d91be2f2ab5630cf58a69c974
William Lindner <wlindner522@gmail.com>
2017-10-08 19:24:23 -0700

    Break out the Note Screen from the App

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-import { LayoutAnimation } from 'react-native';
 import React, { Component } from 'react';
 import Realm from 'realm';
-import styled from 'styled-components/native';
-import uuid from 'uuid';
 
 import Loading from './components/Loading';
-import NoteInput from './components/NoteInput';
-import NoteList from './components/NoteList';
+import NoteScreen from './screens/NoteScreen';
 
 export default class App extends Component {
   state = {
-    expandedInput: false,
     realm: null
   };
 
@@ -28,78 +23,13 @@ export default class App extends Component {
     });
   }
 
-  saveNote = text => {
-    const { realm } = this.state;
-
-    try {
-      realm.write(() => {
-        const newNote = realm.create('Note', {
-          key: uuid.v1(),
-          text,
-          createdAt: new Date()
-        });
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-        this.forceUpdate();
-      });
-    } catch (e) {
-      console.log('Error on creation', e);
-    }
-  };
-
-  deleteNote = key => {
-    const { realm } = this.state;
-    try {
-      realm.write(() => {
-        realm.delete(realm.objectForPrimaryKey('Note', key));
-        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-        this.forceUpdate();
-      });
-    } catch (e) {
-      console.log('Error on creation', e);
-    }
-  };
-
-  expandInput = () => {
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-    this.setState({ expandedInput: true });
-  };
-
-  retractInput = () => {
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-    this.setState({ expandedInput: false });
-  };
-
   render() {
-    const { realm, expandedInput } = this.state;
+    const { realm } = this.state;
 
     if (realm) {
-      const notes = realm.objects('Note').sorted('createdAt', true);
-
-      return (
-        <Container behavior="padding">
-          <NoteInput
-            saveHandler={this.saveNote}
-            onFocusHandler={this.expandInput}
-            loseFocusHandler={this.retractInput}
-          />
-          {!expandedInput && (
-            <NoteListWrapper>
-              <NoteList notes={notes} deleteHandler={this.deleteNote} />
-            </NoteListWrapper>
-          )}
-        </Container>
-      );
+      return <NoteScreen realm={realm} />;
     } else {
       return <Loading />;
     }
   }
 }
-
-const Container = styled.KeyboardAvoidingView`
-  flex: 1;
-  background-color: #f4f4f4;
-`;
-
-const NoteListWrapper = styled.View`
-  flex: 5;
-`;
---
+++ screens/NoteScreen.js
@@ -0,0 +1,85 @@
+import { LayoutAnimation } from 'react-native';
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+import uuid from 'uuid';
+
+import NoteInput from '../components/NoteInput';
+import NoteList from '../components/NoteList';
+
+export default class NoteScreen extends Component {
+  state = {
+    expandedInput: false
+  };
+
+  saveNote = text => {
+    const { realm } = this.props;
+
+    try {
+      realm.write(() => {
+        const newNote = realm.create('Note', {
+          key: uuid.v1(),
+          text,
+          createdAt: new Date()
+        });
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        this.forceUpdate();
+      });
+    } catch (e) {
+      console.log('Error on creation', e);
+    }
+  };
+
+  deleteNote = key => {
+    const { realm } = this.props;
+
+    try {
+      realm.write(() => {
+        realm.delete(realm.objectForPrimaryKey('Note', key));
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        this.forceUpdate();
+      });
+    } catch (e) {
+      console.log('Error on creation', e);
+    }
+  };
+
+  expandInput = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    this.setState({ expandedInput: true });
+  };
+
+  retractInput = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    this.setState({ expandedInput: false });
+  };
+
+  render() {
+    const { realm } = this.props;
+    const { expandedInput } = this.state;
+    const notes = realm.objects('Note').sorted('createdAt', true);
+
+    return (
+      <Container behavior="padding">
+        <NoteInput
+          saveHandler={this.saveNote}
+          onFocusHandler={this.expandInput}
+          loseFocusHandler={this.retractInput}
+        />
+        {!expandedInput && (
+          <NoteListWrapper>
+            <NoteList notes={notes} deleteHandler={this.deleteNote} />
+          </NoteListWrapper>
+        )}
+      </Container>
+    );
+  }
+}
+
+const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  background-color: #f4f4f4;
+`;
+
+const NoteListWrapper = styled.View`
+  flex: 5;
+`;
